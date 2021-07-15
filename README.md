7c220dbe957ce7b98f3b31317ba2fc5311b23c51
hyojin <gywlsh274@gmail.com>
2021-07-15 15:11:34 +0900

    Add: 초기셋팅 완료

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wanted Global Navigation Bar Clone 
+
+원티드 프리온보딩코스 선발 과제입니다.
---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import NavBar from "./Components/NavBar/NavBar";
+import Home from "./Components/Pages/Main/Home";
+
+function Routes() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
